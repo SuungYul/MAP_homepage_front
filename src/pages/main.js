1fc91cd6a773fb525e8413f1615d3bfe11508b31
dongjuyoon <antdny2280@naver.com>
2024-01-09 21:28:49 +0900

    헤더 받기용

--- src/pages/main.js
+++ src/pages/main.js
@@ -7,6 +7,32 @@ const Main = () => {
     <div>
       Main
       <button onClick={() => navigate("/notice")}>notice</button>
+      
+      <button id="LoginButton"onClick={() => navigate("/login")}>Sign in</button>
+
+      <a id="Introduction" > 
+      <p>
+        M J U
+      </p>      
+
+      <p>
+        Computer Engineering
+      </p>
+
+      <p>
+        S T U D Y
+      </p>
+
+      <p>
+      M A P
+      </p>
+
+      </a>
+
+  <img src ="/" alt="맵 사진 1"></img>
+  <img src ="/" alt="맵 사진 2"></img>
+  <img src ="/" alt="맵 사진 3"></img>
+
     </div>
   );
 };
