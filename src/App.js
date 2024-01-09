a1e2c09ba8fcfeb729a7e47db0033d1698116ec4
SuungYul <pmtf00@gmail.com>
2024-01-09 22:17:56 +0900

    FEAT:footer css 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./pages/footer";
 import Router from "./router/router";
 
 function App() {
   return (
     <div className="App">
       <Router />
+      <Footer />
     </div>
   );
 }
