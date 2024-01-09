115ee087eacc0dd1e742e912772d2171c862ed4e
dongjuyoon <antdny2280@naver.com>
2024-01-09 23:47:29 +0900

    d

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,14 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import myimage from "../MAP_image.png" ;
 
 const Main = () => {
   const navigate = useNavigate();
 
-  const backgroundstyle ={
+  const fullscreensection ={
+    height: "100vh",
     backgroundColor: '#000000'
+    
   }
 
   const introductionstyle = {
-
+    position:"absolute",
+    top: "381px",
+    left: "383px",
+    textAlign: "left",
     fontFamily: 'Montserrat',
     fontStyle: 'normal',
     fontWeight: 800,
@@ -17,8 +23,26 @@ const Main = () => {
     letterSpacing: '-0.015em',
     color: '#FFFFFF',
   };
+  const photostyle = {
+    position:"relative",
+    height : '1309px',
+  width: '736px',
+  left: '1200px',
+  top: '80px',
+  backgroundImage: `url(${myimage})`,
+  backgroundRepeat: 'no-repeat'
+  }
+
+  const scrollsection ={
+    
+    backgroundColor: '#000000',
+    height: "50vh"
+    
+  }
 
   const imagestyle ={
+    position: "absolute",
+    bottom: "-100px",
     display:'inclin-flex',
     justifyContent: 'space-evenly',
     padding: '0px',
@@ -27,7 +51,7 @@ const Main = () => {
 
 
   return (
-    <div style={backgroundstyle}>
+    <div style={fullscreensection}>
       Main
       <button onClick={() => navigate("/notice")}>notice</button>
       
@@ -52,11 +76,13 @@ const Main = () => {
 
       </div>
 
-
-    <div style={imagestyle}> 
+<div style={photostyle}></div>
+    <div style={scrollsection}> 
+    <div style={imagestyle}>
   <img src ="/" alt="맵 사진 1"></img>
   <img src ="/" alt="맵 사진 2"></img>
   <img src ="/" alt="맵 사진 3"></img>
+  </div>
   </div>
 
     </div>
