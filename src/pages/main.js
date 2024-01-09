85edc308861ca9eda2ec23c586251b8de92d20fa
dongjuyoon <antdny2280@naver.com>
2024-01-09 22:18:05 +0900

    풀

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,39 @@
 import { useNavigate } from "react-router-dom";
-import "./main.css";
 
 const Main = () => {
   const navigate = useNavigate();
+
+  const backgroundstyle ={
+    backgroundColor: '#000000'
+  }
+
+  const introductionstyle = {
+
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 800,
+    fontSize: '60px',
+    lineHeight: '30px',
+    letterSpacing: '-0.015em',
+    color: '#FFFFFF',
+  };
+
+  const imagestyle ={
+    display:'inclin-flex',
+    justifyContent: 'space-evenly',
+    padding: '0px',
+    width: '100%'
+  }
+
+
   return (
-    <div>
+    <div style={backgroundstyle}>
       Main
       <button onClick={() => navigate("/notice")}>notice</button>
       
       <button id="LoginButton"onClick={() => navigate("/login")}>Sign in</button>
 
-      <a id="Introduction" > 
+      <div style={introductionstyle} > 
       <p>
         M J U
       </p>      
@@ -27,11 +50,14 @@ const Main = () => {
       M A P
       </p>
 
-      </a>
+      </div>
 
+
+    <div style={imagestyle}> 
   <img src ="/" alt="맵 사진 1"></img>
   <img src ="/" alt="맵 사진 2"></img>
   <img src ="/" alt="맵 사진 3"></img>
+  </div>
 
     </div>
   );
