4bf2db0ece670de31365a93b50c8b655c29ae481
SuungYul <pmtf00@gmail.com>
2024-01-18 23:05:58 +0900

    merge

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,2 +1,17 @@
-const Calender = () => {};
-export default Calender;
+import React from "react";
+import "./calendar.css";
+
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+      <div className="Rectangle-ImageBlock"></div>
+      <div className="Rectangle"></div>
+      <div className="Header">
+        <div className="CALENDAR">C A L E N D A R</div>
+        <button className="addButton">일정추가</button>
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/main";
 import Notice from "../pages/notice";
-import Calender from "../pages/calender";
+import Calendar from "../pages/calendar";
 import Gallery from "../pages/gallery";
 import Header from "../pages/header";
 import Footer from "../pages/footer";
@@ -10,11 +10,11 @@ import CallbackPage from "../pages/callback";
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/notice" element={<Notice />} />
-        <Route path="/calender" element={<Calender />} />
+        <Route path="/calendar" element={<Calendar />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/login/oauth2/code/naver" element={<CallbackPage />} />
       </Routes>
