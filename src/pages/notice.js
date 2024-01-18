11e046170e0ac045d5490d5108471b6957b0bdcb
dongjuyoon <antdny2280@naver.com>
2024-01-18 23:05:30 +0900

    노티스 1차

--- src/pages/notice.js
+++ src/pages/notice.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Notice = () => {
@@ -7,10 +7,10 @@ const Notice = () => {
 
     const writeButtonStyle = {
       position: "absolute",
-      width: "7%",
-      left: "42%", // 조절 가능한 위치
-      top: "60%", // 조절 가능한 위치
-      backgroundColor: "#000000",
+      width: "7%", 
+      left: "28%", // 조절 가능한 위치 418
+      top: "20%", // 조절 가능한 위치 177       
+      backgroundColor: "#FFFFFF",
       color: "white",
       padding: "10px 15px",
       border: "1px solid #18A0FB",
@@ -25,21 +25,82 @@ const Notice = () => {
       textAlign: "center",
       letterSpacing: "-0.015em",
       color: "#18A0FB",
+
     };
 
+    const noticeStyle = {
+      position: "absolute",
+      left: "10%", // 조절 가능한 위치 418
+      top: "18%", // 조절 가능한 위치 177      
+      fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '48px',
+    lineHeight: '72px', // or '150%'
+    letterSpacing: '-0.015em',
+    color: '#000000',
+    }
 
-  return
-    (
-      <div>
+    const menuStyle = {
+      position: "absolute",
+      top: "30%", // 조절 가능한 위치 177      
+      left: "70%", // 조절 가능한 위치 418
 
-    <div>notice</div>
-    <button style={writeButtonStyle} onClick={() => navigate("/login")}>
-        Sign In
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '25px',
+      lineHeight: '30px', // or '120%'
+      letterSpacing: '-0.015em',
+      color: '#000000',
+    }
+    
+    const menuStyle2 = {
+      position: "absolute",
+      top: "30%", // 조절 가능한 위치 177      
+      left: "76%", // 조절 가능한 위치 418
+
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '25px',
+      lineHeight: '30px', // or '120%'
+      letterSpacing: '-0.015em',
+      color: '#000000',
+    }
+
+    const menuStyle3 = {
+      position: "absolute",
+      top: "30%", // 조절 가능한 위치 177      
+      left: "84%", // 조절 가능한 위치 418
+
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '25px',
+      lineHeight: '30px', // or '120%'
+      letterSpacing: '-0.015em',
+      color: '#000000',
+    }
+  return(
+    <div>
+
+      <div style={noticeStyle}>N O T I C E</div>
+
+      <button style={writeButtonStyle} onClick={() => navigate("/login")}>
+        글쓰기
       </button>
-      
-      </div>
+
+      <div style={menuStyle}>조회</div>          
+      <div style={menuStyle2}>첨부파일</div>     
+      <div style={menuStyle3}>종류</div>
+
+    </div>
     
   );
   
 };
 export default Notice;
+
+
+
