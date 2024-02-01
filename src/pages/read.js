09805c6c7a54c6c3a9755aa117a21674d2056a9f
dongjuyoon <antdny2280@naver.com>
2024-02-01 18:43:24 +0900

    push

--- src/pages/read.js
+++ src/pages/read.js
@@ -2,24 +2,37 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./read.css";
 
-const Read = () =>{
-    return(
-<div style={{minHeight:"100vh"}}>
-<div className="nbcontainer">
-        <div className="notice">N O T I C E</div>
-        </div>
-        <hr class='hr-solid'/>
-        <hr class='hr-solid2'/>
-        <hr class='hr-solid3'/>
+const Read = () => {
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <div className="Header">
+        <div className="pageTitle">N O T I C E</div>
+      </div>
 
-        <div className="texts" id="addfile">첨부파일</div>     
-
-        <div className="title">제목</div>
-        <div className="content">본문</div>
-        <div className="filename">첨부파일 이름</div>
-</div>
-
-
-    );
+      <div className="readcontentcontainer">
+        <div className="readTitle">제목</div>
+        <div className="line1"></div>
+        <div className="readcontent">본문</div>
+        <div className="line2"></div>
+        <div className="readfile">첨부파일</div>
+        <div className="line3"></div>
+      </div>
+      <div className="commentcontainer">
+        <div className="commentline"></div>
+        <div className="commentname">윤동주 (2024.02.01. 18:01)</div>
+        <div className="commentcontent">dasnlkdnklasdnlkask </div>
+        <div className="commentline2"> </div>
+      </div>
+      <div className="commentwritecontainer">
+        <img className="profilephoto2" alt="프로필사진"></img>
+        <input className="commentInput" type="text"></input>
+      </div>
+      <div className="NameUploadContainer">
+        <div className="commentname2">윤동주</div>
+        <button className="commentwritebutton"> 등록 </button>
+      </div>
+    </div>
+  );
 };
 export default Read;
+// 프로필 사진 추가 해야함
